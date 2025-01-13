c7daa18a93e51aefbf1945e8684e6c403a541b6b
snk988776 <snk988776@gmail.com>
2025-01-13 23:37:40 +0530

    Add files via upload

---
+++ file_input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter the file_name you want to add data to.."
+read file_name
+echo "Enter data for the text file..using cat >> filename command. Press CTR C to stop.."
+cat >> "$file_name"
+
+
+ 
